13b3536883a55848d739a18055871c24a12dbbaf
leedongyeop <ldy_1204@naver.com>
2024-07-21 17:30:27 +0900

    chore : project setting

---
+++ src/main/java/io/dongvelop/springobservability/SpringObservabilityApplication.java
@@ -0,0 +1,13 @@
+package io.dongvelop.springobservability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringObservabilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringObservabilityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/dongvelop/springobservability/SpringObservabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dongvelop.springobservability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringObservabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
